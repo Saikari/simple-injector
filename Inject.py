3bf3ffb96ca66049a5f4cc6ce2c3503514a8aece
さくらみこ <1145793@edu.rut-miit.ru>
2023-08-16 12:18:13 +0300

    Update Inject.py

--- Inject.py
+++ Inject.py
@@ -16,24 +16,28 @@ class Injector:
         self.pid = c_ulong()
         self.handle = None
 
-    def create_process(self, path):
+    def create_process(self, path: str) -> int:
+        """Create a new process and return its PID."""
         return Popen([path]).pid
 
-    def load_from_pid(self, pid):
+    def load_from_pid(self, pid: int) -> None:
+        """Load a handle to an existing process."""
         self.unload()
         self.pid = c_ulong(pid)
         self.handle = self.kernel32.OpenProcess(self.PROC_ALL_ACCESS, 0, pid)
         if not self.handle:
             raise WinError('Unable to load from pid.')
 
-    def unload(self):
+    def unload(self) -> None:
+        """Close the handle to the current process."""
         if self.handle:
             self.kernel32.CloseHandle(self.handle)
             if not self.handle:
                 raise WinError('Unable to unload.')
         self.handle = None
 
-    def alloc_remote(self, buffer, size):
+    def alloc_remote(self, buffer: bytes, size: int) -> int:
+        """Allocate memory in the remote process and return the address."""
         alloc = self.kernel32.VirtualAllocEx(
             self.handle, None, c_int(size),
             self.MEM_CREATE, self.PAGE_EXECUTE_READWRITE
@@ -43,11 +47,13 @@ class Injector:
         self.write_memory(alloc, buffer)
         return alloc
 
-    def free_remote(self, addr, size):
+    def free_remote(self, addr: int, size: int) -> None:
+        """Free memory in the remote process."""
         if not self.kernel32.VirtualFreeEx(self.handle, addr, c_int(0), self.MEM_RELEASE):
             raise WinError('Unable to free remote memory.')
 
-    def get_address_from_module(self, module, function):
+    def get_address_from_module(self, module: str, function: str) -> int:
+        """Get the address of an exported function from a module."""
         module_addr = self.kernel32.GetModuleHandleA(module.encode("ascii"))
         if not module_addr:
             raise WinError('Unable to get module address.')
@@ -56,7 +62,8 @@ class Injector:
             raise WinError('Unable to get function address.')
         return function_addr
 
-    def create_remote_thread(self, function_addr, args):
+    def create_remote_thread(self, function_addr: int, args: bytes) -> int:
+        """Create a remote thread in the remote process."""
         dll_addr = c_long(0)
         args_addr = self.alloc_remote(args, len(args))
         thread = self.kernel32.CreateRemoteThread(
@@ -72,7 +79,8 @@ class Injector:
         self.free_remote(args_addr, len(args))
         return dll_addr.value
 
-    def read_memory(self, addr, size):
+    def read_memory(self, addr: int, size: int) -> bytes:
+        """Read memory from the remote process."""
         old_protect = c_ulong()
         if not self.kernel32.VirtualProtectEx(self.handle, addr, size, self.PAGE_EXECUTE_READWRITE, byref(old_protect)):
             raise WinError('Unable to change memory protection.')
@@ -83,7 +91,8 @@ class Injector:
             raise WinError('Unable to change memory protection.')
         return buffer
 
-    def write_memory(self, addr, string):
+    def write_memory(self, addr: int, string: bytes) -> None:
+        """Write memory to the remote process."""
         size = len(string)
         old_protect = c_ulong()
         if not self.kernel32.VirtualProtectEx(self.handle, addr, size, self.PAGE_EXECUTE_READWRITE, byref(old_protect)):
@@ -93,26 +102,31 @@ class Injector:
         if not self.kernel32.VirtualProtectEx(self.handle, addr, size, old_protect, byref(old_protect)):
             raise WinError('Unable to change memory protection.')
 
-    def load_library(self, buffer):
-        function_addr = self.get_address_from_module("kernel32.dll", "LoadLibraryA")
+    def load_library(self, buffer: bytes) -> int:
+        """Load a library into the remote process."""
+        function_addr = self.get_address_from_module("kernel32.dll", "LoadLibraryW")
         dll_addr = self.create_remote_thread(function_addr, buffer)
         return dll_addr
 
-    def inject_dll(self, path):
+    def inject_dll(self, path: str) -> int:
+        """Inject a DLL into the remote process."""
         self.path = path
-        return self.load_library(path.encode("ascii"))
+        return self.load_library(path.encode("utf-16le"))
 
-    def call_from_injected(self, path, dll_addr, function, args):
-        function_offset = self.get_offset_of_exported_function(path.encode("ascii"), function)
+    def call_from_injected(self, path: str, dll_addr: int, function: str, args: bytes) -> None:
+        """Call a function from the injected DLL."""
+        function_offset = self.get_offset_of_exported_function(path.encode("utf-16le"), function)
         self.create_remote_thread(dll_addr + function_offset, args)
-
-    def get_offset_of_exported_function(self, module, function):
-        base_addr = self.kernel32.LoadLibraryA(module)
+    
+    def get_offset_of_exported_function(self, module: str, function: str) -> int:
+        """Get the offset of an exported function from a module."""
+        base_addr = self.kernel32.LoadLibraryW(module)
         if not base_addr:
             raise WinError('Unable to load library.')
-        function_addr = self.kernel32.GetProcAddress(base_addr, function.encode("ascii"))
+        function_addr = self.kernel32.GetProcAddress(base_addr, function.encode("utf-16le"))
         if not function_addr:
             raise WinError('Unable to get function address.')
         if not self.kernel32.FreeLibrary(base_addr):
             raise WinError('Unable to free library.')
         return function_addr - base_addr
+
