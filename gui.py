cf325c4d4070f2874d23d1572d7eb6f87dd48342
さくらみこ <1145793@edu.rut-miit.ru>
2023-08-16 14:46:15 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,23 +1,17 @@
-from Inject import *
-from subprocess import PIPE, check_output, STDOUT
+from Bypass import *
 from PyQt6.QtWidgets import QMainWindow, QListWidget, QListWidgetItem, QMessageBox, QFileDialog
+import psutil
 from re import findall
-# Подкласс QMainWindow для настройки главного окна приложения
+
 class injectorMW(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Process List")
         self.injectors = {}
         self.processLW = QListWidget(self)
-        self.text = check_output('chcp 65001 & tasklist /FI "Status eq Running" /FO CSV', shell=True, stderr=STDOUT).decode('utf-8').splitlines()[2:]
-        print(self.text)
-        self.processlist = [(ProcessName[1:-1], PID[1:-1]) for 
-        ProcessName, PID, _, _, _ in [x.split(',')
-        for x in self.text]]
-        print(self.processlist)
+        self.processlist = [(p.name(), p.pid) for p in psutil.process_iter(['name', 'pid']) if p.status() == psutil.STATUS_RUNNING]
         self.processNames = [f'{i[0]}\tPID:{i[1]}' for i in self.processlist]
-        print(self.processNames)
-        QLWIs =  [QListWidgetItem(i, self.processLW) for i in self.processNames]
+        QLWIs = [QListWidgetItem(i, self.processLW) for i in self.processNames]
         self.processLW.itemDoubleClicked.connect(self.clickd)
         self.processLW.setSortingEnabled(True)
         self.setCentralWidget(self.processLW)
@@ -25,8 +19,7 @@ class injectorMW(QMainWindow):
     def clickd(self, item):
         pid = findall(r"PID:(.{1,7})", item.text())[0]
         if pid not in self.injectors:
-            fname = QFileDialog.getOpenFileName(self, f'Select .DLL to inject into process {item.text()}')[0]
-            print(fname)
+            fname, _ = QFileDialog.getOpenFileName(self, f'Select .DLL to inject into process {item.text()}')
             if fname:
                 try:
                     self.injectors[pid] = Injector()
@@ -35,12 +28,11 @@ class injectorMW(QMainWindow):
                 except Exception as e:
                     QMessageBox.critical(self, "Error", f'Failed to inject .DLL {self.injectors[pid].path.split("/")[-1]} into {item.text()}')
                 else:
-                    QMessageBox.information(self, "Info", f'Successful injected .DLL {self.injectors[pid].path.split("/")[-1]} into {item.text()}')
+                    QMessageBox.information(self, "Info", f'Successfully injected .DLL {self.injectors[pid].path.split("/")[-1]} into {item.text()}')
         else:
             try:
                 self.injectors[pid].unload()
             except Exception as e:
-                QMessageBox.critical(self, "Error", f'Failed to unload .DLL {self.injectors[pid].path.split("/")[-1]} into {item.text()}')
+                QMessageBox.critical(self, "Error", f'Failed to unload .DLL {self.injectors[pid].path.split("/")[-1]} from {item.text()}')
             else:
-                QMessageBox.information(self, "Info", f'Successful unloaded .DLL {self.injectors[pid].path.split("/")[-1]} into {item.text()}')
-        
+                QMessageBox.information(self, "Info", f'Successfully unloaded .DLL {self.injectors[pid].path.split("/")[-1]} from {item.text()}')
