834ef08114b3454c803f533b8e4e8dcfc56143b7
さくらみこ <1145793@edu.rut-miit.ru>
2023-08-16 13:13:25 +0300

    Update cert_utils.py

--- cert_utils.py
+++ cert_utils.py
@@ -35,28 +35,32 @@ class CertificateGenerator:
         return ''.join(random.choice(letters) for i in range(n))
 
     def GenerateCert(self, domain, inputFile):
-        rootKey = rsa.generate_private_key(public_exponent=65537, key_size=4096)
-        certs, err = self.GetCertificatesPEM(domain + ":443")
-        if err != None:
-            os.chdir("..")
-            foldername = inputFile.split(".")
-            os.rmdir(foldername[0])
-            raise Exception("Error: The domain: " + domain + " does not exist or is not accessible from the host you are compiling on")
-        block = x509.load_pem_x509_certificate(certs.encode(), default_backend())
-        cert = x509.load_pem_x509_certificate(certs.encode(), default_backend())
+        try:
+            rootKey = rsa.generate_private_key(public_exponent=65537, key_size=4096)
+            certs, err = self.GetCertificatesPEM(domain + ":443")
+            if err != None:
+                os.chdir("..")
+                foldername = inputFile.split(".")
+                os.rmdir(foldername[0])
+                raise Exception("Error: The domain: " + domain + " does not exist or is not accessible from the host you are compiling on")
+            block = x509.load_pem_x509_certificate(certs.encode(), default_backend())
+            cert = x509.load_pem_x509_certificate(certs.encode(), default_backend())
+    
+            self.keyToFile(domain+".key", rootKey)
+    
+            SubjectTemplate = x509.CertificateBuilder().subject_name(x509.Name([
+                x509.NameAttribute(NameOID.COMMON_NAME, cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value)
+            ])).issuer_name(x509.Name([
+                x509.NameAttribute(NameOID.COMMON_NAME, cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value)
+            ])).not_valid_before(cert.not_valid_before).not_valid_after(cert.not_valid_after).serial_number(cert.serial_number).public_key(rootKey.public_key()).add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True).add_extension(x509.KeyUsage(digital_signature=True, key_cert_sign=True, key_encipherment=True, content_commitment=True, data_encipherment=True), critical=True).add_extension(x509.ExtendedKeyUsage([x509.ExtendedKeyUsageOID.SERVER_AUTH, x509.ExtendedKeyUsageOID.CLIENT_AUTH]), critical=True).sign(rootKey, hashes.SHA256(), default_backend())
+            IssuerTemplate = x509.CertificateBuilder().subject_name(x509.Name([
+                x509.NameAttribute(NameOID.COMMON_NAME, cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value)
+            ])).not_valid_before(cert.not_valid_before).not_valid_after(cert.not_valid_after).serial_number(cert.serial_number).public_key(cert.public_key()).add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True).add_extension(x509.KeyUsage(digital_signature=True, key_cert_sign=True, key_encipherment=True, content_commitment=True, data_encipherment=True), critical=True).add_extension(x509.ExtendedKeyUsage([x509.ExtendedKeyUsageOID.SERVER_AUTH, x509.ExtendedKeyUsageOID.CLIENT_AUTH]), critical=True).sign(rootKey, hashes.SHA256(), default_backend())
+            derBytes = SubjectTemplate.public_bytes(serialization.Encoding.DER)
+            self.certToFile(domain+".pem", derBytes)
+        except Exception as e:
+            print("An error occurred during certificate generation:", str(e))
 
-        self.keyToFile(domain+".key", rootKey)
-
-        SubjectTemplate = x509.CertificateBuilder().subject_name(x509.Name([
-            x509.NameAttribute(NameOID.COMMON_NAME, cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value)
-        ])).issuer_name(x509.Name([
-            x509.NameAttribute(NameOID.COMMON_NAME, cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value)
-        ])).not_valid_before(cert.not_valid_before).not_valid_after(cert.not_valid_after).serial_number(cert.serial_number).public_key(rootKey.public_key()).add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True).add_extension(x509.KeyUsage(digital_signature=True, key_cert_sign=True, key_encipherment=True, content_commitment=True, data_encipherment=True), critical=True).add_extension(x509.ExtendedKeyUsage([x509.ExtendedKeyUsageOID.SERVER_AUTH, x509.ExtendedKeyUsageOID.CLIENT_AUTH]), critical=True).sign(rootKey, hashes.SHA256(), default_backend())
-        IssuerTemplate = x509.CertificateBuilder().subject_name(x509.Name([
-            x509.NameAttribute(NameOID.COMMON_NAME, cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value)
-        ])).not_valid_before(cert.not_valid_before).not_valid_after(cert.not_valid_after).serial_number(cert.serial_number).public_key(cert.public_key()).add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True).add_extension(x509.KeyUsage(digital_signature=True, key_cert_sign=True, key_encipherment=True, content_commitment=True, data_encipherment=True), critical=True).add_extension(x509.ExtendedKeyUsage([x509.ExtendedKeyUsageOID.SERVER_AUTH, x509.ExtendedKeyUsageOID.CLIENT_AUTH]), critical=True).sign(rootKey, hashes.SHA256(), default_backend())
-        derBytes = SubjectTemplate.public_bytes(serialization.Encoding.DER)
-        self.certToFile(domain+".pem", derBytes)
 
     def keyToFile(self, filename, key):
         with open(filename, "wb") as file:
@@ -67,15 +71,19 @@ class CertificateGenerator:
             file.write(derBytes)
 
     def GetCertificatesPEM(self, address):
-        conn = ssl.create_connection((address, 443))
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-        sock = context.wrap_socket(conn, server_hostname=address)
-        certs = sock.getpeercert(True)
-        sock.close()
-        b = b""
-        for cert in certs:
-            b += cert[1]
-        return b.decode(), None
+        try:
+            conn = ssl.create_connection((address, 443))
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+            sock = context.wrap_socket(conn, server_hostname=address)
+            certs = sock.getpeercert(True)
+            sock.close()
+            b = b""
+            for cert in certs:
+                b += cert[1]
+            return b.decode(), None
+        except (ssl.SSLError, ConnectionRefusedError, socket.gaierror, socket.timeout) as e:
+            raise Exception(f"Error getting certificates for {address}: {str(e)}")
+
 
     def GeneratePFK(self, password, domain):
         key = crypto.PKey()
