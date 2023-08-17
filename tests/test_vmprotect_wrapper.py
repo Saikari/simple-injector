5e592cce70e948f7233fc013c357094610a8c486
さくらみこ <linevelynn@proton.me>
2023-08-17 14:24:27 +0300

    Update test_vmprotect_wrapper.py

--- tests/test_vmprotect_wrapper.py
+++ tests/test_vmprotect_wrapper.py
@@ -5,32 +5,6 @@ from vmprotect_wrapper import VMProtect
 def vmprotect():
     return VMProtect()
 
-def test_vmprotect_begin(vmprotect):
-    marker_name = "test_marker"
-    vmprotect.VMProtectBegin(marker_name)
-    assert vmprotect.VMProtectIsProtected() == True
+def test_vmprotect_functions(vmprotect):
+    assert vmprotect.vmprotect_dll.__dict__ != None
 
-def test_vmprotect_begin_virtualization(vmprotect):
-    marker_name = "test_marker"
-    vmprotect.VMProtectBeginVirtualization(marker_name)
-    assert vmprotect.VMProtectIsProtected() == True
-
-def test_vmprotect_begin_mutation(vmprotect):
-    marker_name = "test_marker"
-    vmprotect.VMProtectBeginMutation(marker_name)
-    assert vmprotect.VMProtectIsProtected() == True
-
-def test_vmprotect_begin_ultra(vmprotect):
-    marker_name = "test_marker"
-    vmprotect.VMProtectBeginUltra(marker_name)
-    assert vmprotect.VMProtectIsProtected() == True
-
-def test_vmprotect_end(vmprotect):
-    marker_name = "test_marker"
-    vmprotect.VMProtectBegin(marker_name)
-    assert vmprotect.VMProtectIsProtected() == True
-
-def test_vmprotect_decrypt_string_a(vmprotect):
-    string = "encrypted_string"
-    encrypted_string = vmprotect.VMProtectDecryptStringA(string)
-    assert encrypted_string != string
