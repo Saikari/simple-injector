4602378e7f2075b1a0e235c057da87d13ddcb522
さくらみこ <linevelynn@proton.me>
2023-08-17 14:54:32 +0300

    Update test_CertificateGenerator.py

--- tests/test_CertificateGenerator.py
+++ tests/test_CertificateGenerator.py
@@ -1,5 +1,5 @@
 from os import path
-from pytest import fixture
+from pytest import fixture, capfd
 from CertificateGenerator import CertificateGenerator
 
 
@@ -36,6 +36,6 @@ def test_check_signature_verification(cert_gen):
     cert_gen.real = "dzen.ru.pfx"
     cert_gen.password = "password"
     cert_gen.Check(fileout)
-    captured = capsys.readouterr()
-    assert "Signature verified successfully." == captured.out
+    out, err = capfd.readouterr()
+    assert "Signature verified successfully." in out
 
