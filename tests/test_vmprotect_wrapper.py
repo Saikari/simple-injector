eb3ef1550d9123b4412ca6905c406a6727d7788e
さくらみこ <linevelynn@proton.me>
2023-08-17 14:30:24 +0300

    Update test_vmprotect_wrapper.py

--- tests/test_vmprotect_wrapper.py
+++ tests/test_vmprotect_wrapper.py
@@ -1,11 +1,12 @@
 import pytest
 from vmprotect_wrapper import VMProtect
 
+
 @pytest.fixture
 def vmprotect():
     return VMProtect()
 
+
 def test_vmprotect_functions(vmprotect):
     print(vmprotect.vmprotect_dll.__dict__)
-    assert vmprotect.vmprotect_dll.__dict__ != None
-
+    assert vmprotect.vmprotect_dll.__dict__ is not None
